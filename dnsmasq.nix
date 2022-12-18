40fad18630e62284b89654c2737454b130769c99
Matt Wittmann <mcwitt@gmail.com>
2022-12-18 15:08:33 -0800

    Update for new options

--- dnsmasq.nix
+++ dnsmasq.nix
@@ -8,40 +8,39 @@ in
   services.dnsmasq =
     {
       enable = true;
-      servers =
-        let
-          cloudflare = [
-            # https://developers.cloudflare.com/1.1.1.1/setup/linux/
-            "1.1.1.1"
-            "1.0.0.1"
-            "2606:4700:4700::1111"
-            "2606:4700:4700::1001"
-          ];
 
-          google = [
-            # https://developers.google.com/speed/public-dns/docs/using#linux
-            "8.8.8.8"
-            "8.8.4.4"
-            "2001:4860:4860::8888"
-            "2001:4860:4860::8844"
-          ];
+      settings = {
+        server =
+          let
+            cloudflare = [
+              # https://developers.cloudflare.com/1.1.1.1/setup/linux/
+              "1.1.1.1"
+              "1.0.0.1"
+              "2606:4700:4700::1111"
+              "2606:4700:4700::1001"
+            ];
 
-        in
-        lib.concatLists [ cloudflare google ];
+            google = [
+              # https://developers.google.com/speed/public-dns/docs/using#linux
+              "8.8.8.8"
+              "8.8.4.4"
+              "2001:4860:4860::8888"
+              "2001:4860:4860::8844"
+            ];
 
-      extraConfig = ''
-        domain-needed
-        bogus-priv
-        no-resolv
-        no-poll
-        log-queries
+          in
+          lib.concatLists [ cloudflare google ];
 
-        cache-size=10000
-        local-ttl=300
-        log-facility=${logfile}
-
-        conf-file=${pkgs.blocked-hosts}
-      '';
+        domain-needed = true;
+        bogus-priv = true;
+        no-resolv = true;
+        no-poll = true;
+        log-queries = true;
+        cache-size = 10000;
+        local-ttl = 300;
+        log-facility = logfile;
+        conf-file = toString pkgs.blocked-hosts;
+      };
     };
 
   services.logrotate = {
