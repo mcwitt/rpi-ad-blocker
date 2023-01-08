97e7eee453300cd2f42f3b295924d5dd37d4d60f
Matt Wittmann <mcwitt@gmail.com>
2023-01-08 11:36:35 -0800

    Clean, add pre-commit

--- dnsmasq.nix
+++ dnsmasq.nix
@@ -1,48 +1,47 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, blocked-hosts, ... }:
 let logfile = "/var/log/dnsmasq.log";
 in
 {
   networking.firewall.allowedTCPPorts = [ 53 ];
   networking.firewall.allowedUDPPorts = [ 53 ];
 
-  services.dnsmasq =
-    {
-      enable = true;
-      servers =
-        let
-          cloudflare = [
-            # https://developers.cloudflare.com/1.1.1.1/setup/linux/
-            "1.1.1.1"
-            "1.0.0.1"
-            "2606:4700:4700::1111"
-            "2606:4700:4700::1001"
-          ];
+  services.dnsmasq = {
+    enable = true;
+    servers =
+      let
+        cloudflare = [
+          # https://developers.cloudflare.com/1.1.1.1/setup/linux/
+          "1.1.1.1"
+          "1.0.0.1"
+          "2606:4700:4700::1111"
+          "2606:4700:4700::1001"
+        ];
 
-          google = [
-            # https://developers.google.com/speed/public-dns/docs/using#linux
-            "8.8.8.8"
-            "8.8.4.4"
-            "2001:4860:4860::8888"
-            "2001:4860:4860::8844"
-          ];
+        google = [
+          # https://developers.google.com/speed/public-dns/docs/using#linux
+          "8.8.8.8"
+          "8.8.4.4"
+          "2001:4860:4860::8888"
+          "2001:4860:4860::8844"
+        ];
 
-        in
-        lib.concatLists [ cloudflare google ];
+      in
+      lib.concatLists [ cloudflare google ];
 
-      extraConfig = ''
-        domain-needed
-        bogus-priv
-        no-resolv
-        no-poll
-        log-queries
+    extraConfig = ''
+      domain-needed
+      bogus-priv
+      no-resolv
+      no-poll
+      log-queries
 
-        cache-size=10000
-        local-ttl=300
-        log-facility=${logfile}
+      cache-size=10000
+      local-ttl=300
+      log-facility=${logfile}
 
-        conf-file=${pkgs.blocked-hosts}
-      '';
-    };
+      conf-file=${blocked-hosts}
+    '';
+  };
 
   services.logrotate = {
     enable = true;
--- flake.nix
+++ flake.nix
@@ -2,39 +2,47 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     hosts-blocklists = {
       url = "github:notracking/hosts-blocklists";
       flake = false;
     };
   };
 
-  outputs = inputs @ { self, nixpkgs, flake-utils, ... }: {
+  outputs =
+    inputs @ { self
+    , nixpkgs
+    , flake-utils
+    , pre-commit-hooks
+    , ...
+    }: {
 
-    overlay = final: _: {
-      sanitize-blocklist = final.haskellPackages.callPackage ./sanitize-blocklist { };
-
-      blocked-hosts = final.runCommand "blocked-hosts" { } ''
-        ${final.sanitize-blocklist} < ${inputs.hosts-blocklists}/dnsmasq/dnsmasq.blacklist.txt > $out
-      '';
-    };
-
-    nixosConfigurations.pinto = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        { nixpkgs.overlays = [ self.overlay ]; }
-        ./configuration.nix
-      ];
-      specialArgs = { inherit inputs; };
-    };
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
+      nixosConfigurations.pinto = nixpkgs.lib.nixosSystem rec {
+        system = "aarch64-linux";
+        modules = [ ./configuration.nix ];
+        specialArgs = { inherit (self.packages.${system}) blocked-hosts; };
       };
+    } // flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
     in
     rec {
-      packages = { inherit (pkgs) blocked-hosts sanitize-blocklist; };
+      packages = rec {
+        sanitize-blocklist = pkgs.haskellPackages.callPackage ./sanitize-blocklist { };
+
+        blocked-hosts = pkgs.runCommand "blocked-hosts" { } ''
+          ${sanitize-blocklist} < ${inputs.hosts-blocklists}/dnsmasq/dnsmasq.blacklist.txt > $out
+        '';
+      };
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          excludes = [ "hardware-configuration\\.nix" ];
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      };
 
       devShells.default = pkgs.mkShell {
         inputsFrom = [ packages.sanitize-blocklist ];
@@ -42,6 +50,7 @@
           haskellPackages.haskell-language-server
           ormolu
         ];
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
       };
     });
 }
